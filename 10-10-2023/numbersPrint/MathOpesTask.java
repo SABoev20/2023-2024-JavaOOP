adec4c817da1df4657cbbd4a4d42993fa5947030
Симеон Асенов Боев <SABoev20@codingburgas.bg>
2023-10-10 10:11:40 +0300

    Commit Task3

---
+++ 10-10-2023/numbersPrint/MathOpesTask.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MathOpesTask {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+
+        System.out.println(a + b);
+    }
+}
