33fdb218d06295495078dbc0d0c90c8875acb68c
Симеон Асенов Боев <SABoev20@codingburgas.bg>
2023-10-17 09:39:33 +0300

    Fix code

--- 11-10-2023/GuessNum.java
+++ 11-10-2023/GuessNum.java
@@ -5,15 +5,22 @@ public class GuessNum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int rNum = random.nextInt(10);
+        int rNum = random.nextInt(100);
 
         while (true){
             System.out.print("Enter a number: ");
-            if(scanner.nextInt() == rNum){
+            int num = scanner.nextInt();
+            if(num == rNum){
                 System.out.println("You guessed the number!");
                 break;
             }
-            System.out.println("Wrong number, please try again!");
+            else if(num < rNum){
+                System.out.println("The number is higher than " + num);
+            }
+            else if (num > rNum) {
+                System.out.println("The number is lower than " + num);
+            }
+            System.out.println("Try again!");
         }
     }
 }
--- 11-10-2023/Temperature.java
+++ 11-10-2023/Temperature.java
@@ -3,6 +3,35 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextFloat() * 1.8 + 32);
+
+        String input = "";
+        char type = ' ';
+
+        while (true) {
+            System.out.print("Enter temperature (with C or F): ");
+            input = scanner.next();
+            type = input.charAt(input.length() - 1);
+            if (type != 'C' && type != 'F'){
+                System.out.println("Wrong or no temperature scale symbol entered!");
+                System.out.println("Please try again");
+                continue;
+            }
+            break;
+        }
+
+        String valueInStr = "";
+        for(int i = 0; i < input.length() - 1; i++)
+        {
+            valueInStr += input.charAt(i);
+        }
+        float valueFloat = Float.parseFloat(valueInStr);
+        if(type == 'C'){
+            float temp = (valueFloat * 1.8f) + 32;
+            System.out.println("The value of " + input + " in Fahrenheit is: " + temp);
+        }
+        else{
+            float temp = ((valueFloat - 32) *5) / 9;
+            System.out.println("The value of " + input + " in Celsius is: " + temp);
+        }
     }
 }
